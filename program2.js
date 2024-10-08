eaec5ba0636993a6f48504d802a749c46c3dc157
nikhilsaha28 <nikhilsaha530@gmail.com>
2024-10-08 14:14:15 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,31 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let total = 0; 
+    let prevValue = 0; 
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]]; 
 
-module.exports={romanToInt}
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+
+        prevValue = currentValue;
+    }
+
+    return total; 
+}
+
+console.log(romanToInt("III"));      
+console.log(romanToInt("LVIII"));    
+console.log(romanToInt("MCMXCIV")); 
